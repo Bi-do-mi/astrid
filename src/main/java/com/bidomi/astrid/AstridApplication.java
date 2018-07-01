3490f5b19115c35010083f3649338b8d4562b060
Marat <bidomi@mail.ru>
2018-07-01 10:49:51 +0300

    1

---
+++ src/main/java/com/bidomi/astrid/AstridApplication.java
@@ -0,0 +1,12 @@
+package com.bidomi.astrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstridApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstridApplication.class, args);
+    }
+}
